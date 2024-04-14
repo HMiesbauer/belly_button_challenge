bedc5f1d7656daaa46dfd969cdb868e6d77b470f
HMiesbauer <150979374+HMiesbauer@users.noreply.github.com>
2024-04-14 10:52:17 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,73 +1,11 @@
-let globalData = [];
+const URL = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
-//use d3 to get file
-d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json")
-.then(data => createPlot(data));
 
-//function to create plot
-
-function createPlot(data){
-    let names = data.names;
-    //console.log(data);
-    
-    globalData.push(data);
-
-    let samples = data.samples;
-    let otuId1 = samples[0].otu_ids.slice(0, 10);
-    let otuLabel1 = samples[0].otu_labels.slice(0, 10);
-    let sample1 = samples[0].sample_values.slice(0,10);
-    //console.log(sample1);
-
-    createMenu(names)
-
-//create bar chart
- let trace1 ={
-    x: sample1,
-    y: otuId1.map(id => `OTU ${id}`),
-    text: otuLabel1,
-    type: "bar",
-    orientation: "h"
-
- };
-
-//define layout and show plot
-let layout1 = {
-    title: ""
-};
-
-Plotly.newPlot("bar", trace1, layout1);
-
-//create bubble chart
-let trace2 ={
-
-    x: otuId1,
-    y: sample1,
-    text: otuLabel1,
-    mode: 'markers',
-    marker: {
-        size: sample1,
-        //sizemode: 'area',
-        color: otuId1,
-        colorscale: 'Earth',
-}
-}
-
-//bubblechart plot and layout
-let layout2 = {
-    title: "",
-    showlegend: false,
-    height: 600,
-    width: 600,
-    
-};
-
-Plotly.newPlot("bubble", trace2, layout2);
-
-};
+let globalData; 
 
 //function to create menu
-function createMenu(names){
-    d3.select("select")
+function createMenu(names) {
+    d3.select("#selDataset")
         .selectAll("option")
         .data(names)
         .enter()
@@ -77,23 +15,106 @@ function createMenu(names){
 }
 
 
+function init(data) {
+    globalData = data;
+    let names = globalData.names;
+    createMenu(names);
+    changeSubject(names[0]);
 
-//function for menu drop down list + metadata
-function changeSubject(subjectId){
-    console.log(subjectId);
-    let metadata1 = globalData[0].metadata.find(item => item.id == subjectId);
+
+}
+
+function updateMeta(subjectId) {
+    let subjectMeta = globalData.metadata.find(item => item.id == subjectId);
     //console.log("Metadata for selected id:", metadata1);
-    console.log("Selected subject ID:", subjectId);
+    // console.log("Selected subject ID:", subjectId);
 
     //display metadata for test subject id
-    d3.select("#sample-metadata").html("");
+    let metaBox = d3.select("#sample-metadata");
+    metaBox.html("");
 
-    let metadata2 = d3.select("#sample-metadata");
-    Object.entries(metadata1).forEach(([key, value]) => {
-        metadata2.append("p").text(`${key}: ${value}`);
+    Object.entries(subjectMeta).forEach(([key, value]) => {
+        metaBox.append("p").text(`${key}: ${value}`);
     });
+}
 
+function updateBarChart(subjectSamples) {
+    let sample1 = subjectSamples.sample_values.slice(0, 10).reverse();
+    let otuLabel1 = subjectSamples.otu_labels.slice(0, 10).reverse();
+    let otuId1 = subjectSamples.otu_ids.slice(0, 10).reverse();
+        let trace1 = {
+            x: sample1,
+            y: otuId1.map(id => `OTU ${id}`),
+            text: otuLabel1,
+            type: "bar",
+            orientation: "h"
 };
+    
+    //define layout and show plot
+        let layout1 = {
+            title: ""
+    };
+    
+    // console.log(otuId1.map(id => `OTU ${id}`));
+      
+    Plotly.newPlot("bar", [trace1], layout1);
+    
+};
+    
+
+// function changeSubject(subjectId){
+//     // console.log(subjectId);
+   
+//     updateMeta(subjectId);
+//     let subjectSamples = globalData.samples.find(item => item.id == subjectId);
+//     updateBarChart(subjectSamples);
+// };
+
+
+//create bubble chart
+function updateBubbleChart(subjectSamples) {
+
+    console.log(globalData.samples); // Add this line
+   
+
+    let sample1 = subjectSamples.sample_values;
+    let otuLabel1 = subjectSamples.otu_labels;
+    let otuId1 = subjectSamples.otu_ids;
+
+        let trace2 = {
+
+            x: otuId1,
+            y: sample1,
+            text: otuLabel1,
+            mode: 'markers',
+            marker: {
+                size: sample1,
+                //sizemode: 'area',
+                color: otuId1,
+                colorscale: 'Earth',
+    }
+};
+
+//bubblechart plot and layout
+    let layout2 = {
+        title: "",
+        showlegend: false,
+        height: 600,
+        width: 600,
+};
+    Plotly.newPlot("bubble", [trace2], layout2);
+};
+
+function changeSubject(subjectId) {
+    updateMeta(subjectId);
+    let subjectSamples = globalData.samples.find(item => item.id == subjectId);
+    updateBarChart(subjectSamples);
+    updateBubbleChart(subjectSamples);
+};
+
+
+//use d3 to get file
+d3.json(URL).then(init);
 
 
 
